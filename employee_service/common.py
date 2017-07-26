ce11d9478b02e6f916be98069a4535a2c3461273
Szymon Michalski <szyy89@gmail.com>
2017-07-26 15:54:04 +0200

    Fixed getting all employees when filter is None

--- employee_service/common.py
+++ employee_service/common.py
@@ -4,6 +4,10 @@ from employee_service.application import db
 def get_employees(email):
     from employee_service.models import \
         Employee
-    employees = list(
-        db.session.query(Employee).filter_by(email=email))
+    if email:
+        employees = list(
+            db.session.query(Employee).filter_by(email=email))
+    else:
+        employees = list(
+            db.session.query(Employee).all())
     return employees
